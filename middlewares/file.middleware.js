fb3cd2e4afaa1e36eecd3195369735f0e99e1798
aortizt <aortizt@sanitas.es>
2022-12-16 19:20:44 +0100

    add upload image to cloudinary with multer

--- middlewares/file.middleware.js
+++ middlewares/file.middleware.js
@@ -1,19 +1,22 @@
 const path = require('path');
 const multer = require('multer');
+const { Readable } = require("stream");
+const sharp = require("sharp");
 
 // Importaremos las librerías necesarias para la nueva función
 const fs = require('fs');
 const cloudinary = require('cloudinary').v2;
 
-const storage = multer.diskStorage({
-    filename: (req, file, cb) => {
-        console.log('filename ->', file);
-        cb(null, `${Date.now()}-${file.originalname}`);
-    },
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../tmp'))
-    },
-});
+const storage = multer.memoryStorage();
+// const storage = multer.diskStorage({
+//     filename: (req, file, cb) => {
+//         console.log('filename ->', file);
+//         cb(null, `${Date.now()}-${file.originalname}`);
+//     },
+//     destination: (req, file, cb) => {
+//         cb(null, path.join(__dirname, '../tmp'))
+//     },
+// });
 
 const VALID_FILE_TYPES = ['image/png', 'image/jpg', 'image/jpeg'];
 
@@ -27,29 +30,32 @@ const fileFilter = (req, file, cb) => {
 };
 
 const upload = multer({
-    storage,
+    // storage,
+    storage: storage,
     fileFilter,
 });
 
 // Ahora tenemos un nuevo middleware de subida de archivos
 const uploadToCloudinary = async (req, res, next) => {
-	if (req.file) {
-    try{
-		const filePath = req.file.path;
-    const image = await cloudinary.uploader.upload(filePath);
-
-		// Borramos el archivo local
-    await fs.unlinkSync(filePath);
-	
-		// Añadimos la propiedad file_url a nuestro Request
-    req.file_url = image.secure_url;
-		return next();
-    }catch(error){
-      return next(error)
+	const bufferToStream = (buffer) => {
+    const readable = new Readable({
+      read() {
+        this.push(buffer);
+        this.push(null);
+      },
+    });
+    return readable;
+  };
+  const data = await sharp(req.file.buffer).toBuffer();
+  const stream = cloudinary.uploader.upload_stream(
+    { folder: "DEV" },
+    (error, result) => {
+      if (error) return next(error);
+      req.file_url = result.secure_url;
+      return next();
     }
-  } else {
-    return next();
-  }
+  );
+  bufferToStream(data).pipe(stream);
 };
 
 module.exports = { upload: upload, uploadToCloudinary };
