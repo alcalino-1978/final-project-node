fd1beaf4da210602646756279f241e595df915f0
alberto <user@MacBook-Pro-de-USER.local>
2023-02-13 11:32:33 +0100

    Add fields(name, lastname, phone) to user model

--- models/User.js
+++ models/User.js
@@ -6,9 +6,9 @@ const userSchema = new Schema(
     {
       email: { type: String, required: true },
       password: { type: String, required: true },
-      //name: { type: String, required: true }, si quisieramos añadir un nombre de usuario
-      //lastName: { type: String, required: true }, 
-      //phoneNumber:{ type: String, required: true },
+      name: { type: String, required: true },
+      lastName: { type: String, required: true }, 
+      phoneNumber:{ type: String, required: true },
       //....
     },
     {
