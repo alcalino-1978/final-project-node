10e776b638a2c7c9e7fc2903d662a9385dd582b6
alberto <user@MacBook-Pro-de-USER.local>
2023-02-16 20:01:42 +0100

    change cors origin

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors'); 
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+require("dotenv").config();
 
 // Auth
-require('jsonwebtoken'); // Requerimos nuestro archivo de configuración
+require("jsonwebtoken"); // Requerimos nuestro archivo de configuración
 
 // Utils
-const { connect } = require('./utils/db');
-
+const { connect } = require("./utils/db");
 
 // Server config
 connect();
@@ -16,12 +15,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Routes config
-const patientsRoutes = require('./routes/patient.routes');
-const doctorsRoutes = require('./routes/doctor.routes');
-const userRoutes = require('./routes/user.routes');
+const patientsRoutes = require("./routes/patient.routes");
+const doctorsRoutes = require("./routes/doctor.routes");
+const userRoutes = require("./routes/user.routes");
 
 //CORS CONFIG
-const whitelist = ['http://localhost:4200']; //white list consumers
+const whitelist = ["http://localhost:4200"]; //white list consumers
 var corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -30,19 +29,27 @@ var corsOptions = {
       callback(null, false);
     }
   },
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "device-remember-token",
+    "Access-Control-Allow-Origin",
+    "Origin",
+    "Accept",
+  ],
 };
 // const insuranceRoutes = require('./routes/insurance.routes');
 
-app.get('/', (req, res) => {
-    // res.send('<h1>Hello World!</h1>');
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  // res.send('<h1>Hello World!</h1>');
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 
 // Middlewares config
 app.use(express.json());
@@ -54,27 +61,33 @@ app.use(express.urlencoded({ extended: true }));
 //   res.header('Access-Control-Allow-Origin', '*');
 //   next();
 // });
-app.use(cors(corsOptions));
-app.set('secretKey', 'nodeRestApi'); // Config JWT
+// app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "http://localhost:4200",
+  })
+);
+app.set("secretKey", "nodeRestApi"); // Config JWT
 
 //Routes
-app.use('/patients', patientsRoutes);
-app.use('/doctors', doctorsRoutes);
+app.use("/patients", patientsRoutes);
+app.use("/doctors", doctorsRoutes);
 // app.use('/insurances', insuranceRoutes);
-app.use('/users', userRoutes);
-
+app.use("/users", userRoutes);
 
 // Control error
-app.use('*', (req, res, next) => {
-    const error = new Error('Route not found');
-    error.status = 404;
-    next(error);
-})
+app.use("*", (req, res, next) => {
+  const error = new Error("Route not found");
+  error.status = 404;
+  next(error);
+});
 app.use((error, req, res, next) => {
-    return res.status(error.status || 500).json(error.message || 'Unexpected error');
-})
+  return res
+    .status(error.status || 500)
+    .json(error.message || "Unexpected error");
+});
 
 // Listen Server
 app.listen(PORT, () => {
-    console.log(`Server running in http://localhost:${PORT}`);
-})
+  console.log(`Server running in http://localhost:${PORT}`);
+});
